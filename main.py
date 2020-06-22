ad9b2cdddd4d4e565d1cc0b45459a236c58505d4
shwetabaskaran <39169461+shwetabaskaran@users.noreply.github.com>
2020-06-22 14:54:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+import os
+
+ 
+
+#iport = int(os.getenv('PORT', 8000))
+@app.route('/')
+def home():
+    if conn:
+        return render_template('home.html')
+
+ 
+
+
+if __name__ == '__main__':
+    print(iport)
+    #app.run(host='0.0.0.0', port=iport,debug = False)
+    app.run()
